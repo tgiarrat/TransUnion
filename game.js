006c509f9fbfa98a2daa9ef6c0cd0277f92cb3ae
Jackson Darrow <jackson.darrow@gmail.com>
2016-05-07 01:11:26 -0700

    a commit

--- game.js
+++ game.js
@@ -7,8 +7,6 @@ var money = 5000;
 var creditScore = 500;
 
 //getPossibleLoans();
-var testLoan = new Loan("test", 10, 10000, 0.5, 10);
-testLoan.init.call(testLoan, (new Date()).getTime() + 1000, $(".buttons"));
 
 
 function getPossibleLoans() {
@@ -135,7 +133,63 @@ function Task(name, money, time, requirements, domParent) {
     };
 }
 
+function Task(parentDom, time, rewardMoney, rewardCS, cost = 0,oneTime = false, skillReq = 0){
+    this.parentDom = parentDom;
+    this.skillReq = skillReq;
+    this.oneTime = oneTime;
+    this.time = time;
+    this.rewardMoney = rewardMoney;
+    this.rewardCS = rewardCS;
+    this.running = false;
+    
+    this.init = function() {
+        //show self
+    };
+    
+    this.start = function() {
+        if (!this.running) {
+            money -= this.cost;
+            this.running = true;
+            setTimeout(this.time, this.end);
+        }
+    };
 
+    this.end = function(){
+        money += rewardMoney();
+        rewardCS.go();
+        if (oneTime)
+        {
+            //hide myself
+        }
+        
+        this.running = false;
+    };
+}
 
+function creditUpdate(type, action, creditDelta = 0) {
+    this.type = type;
+    this.action = action;
 
-
+    data[key] = value;
+    
+    this.go = function() {
+        if (creditDelta === 0){
+            $.ajax({
+                dataType: "json",
+                contentType: "application/json",
+                url: "http://ec2-52-53-177-180.us-west-1.compute.amazonaws.com/score-simulator/scoresim/simulateScore",
+                type: "POST",
+                data: {
+                    "score": score,
+                    "event": {
+                        this.type: this.action
+                    }
+                },
+                success: function(res) { score = res.score; },
+                error: function(res) { console.warn(res); }
+            });
+        }
+        else
+            credit += creditDelta;
+    }
+}
