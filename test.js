34451d53836ce123eb75293a942578a1d0201975
Kyle <kylem3r@gmail.com>
2016-05-07 01:36:02 -0700

    drawing a loan

--- test.js
+++ test.js
@@ -1,58 +1,21 @@
-function Task(parentDom, time, rewardMoney, rewardCS, cost = 0,oneTime = false, skillReq = 0){
-    this.parentDom = parentDom;
-    this.skillReq = skillReq;
-    this.oneTime = oneTime;
-    this.time = time;
-    this.rewardMoney = rewardMoney;
-    this.rewardCS = rewardCS;
-    this.running = false;
-    
-    this.init = function() {
-        //show self
-    }
-    
-    this.start = function() {
-        if (!this.running) {
-            money -= this.cost;
-            this.running = true;
-            setTimeout(this.time, this.end);
-        }
-    }
-    this.end = function(){
-        money += rewardMoney();
-        rewardCS.go();
-        if (oneTime)
-        {
-            //hide myself
-        }
-        
-        this.running = false;
-    }
-}
+function Loan(name, amount, length, dpr, numPayments, payment) {
+    console.log("[LOAN] creating loan");
 
-function creditUpdate(type, action, creditDelta = 0)
-{
-    this.type = type;
-    this.action = action;
+    this.name = name; // name of loan eg: "car loan"
+    this.amount = amount;
+    this.balance = amount;
+    this.payment = payment = payment | 0;
+
+    this.length = length;
+    this.numPayments = numPayments;
+    this.period = length / numPayments;
+    this.dpr = dpr;
     
-    this.go = function() {
-        if (creditDelta == 0){
-            $.ajax({
-                dataType: "json",
-                contentType: "application/json",
-                url: "http://ec2-52-53-177-180.us-west-1.compute.amazonaws.com/score-simulator/scoresim/simulateScore",
-                type: "POST",
-                data: {
-                    "score": score,
-                    "event": {
-                        this.type : this.action
-                    }
-                },
-                success: function(res) { score = res.score;  },
-                error:   function(res) { console.warn(res); }
-            });
-        }
-        else
-            credit += creditDelta;
+    this.draw = function(){
+        this.dom = $("<li><div>" +
+                             "Loan " + name + " - Balance: " + balance +
+                             "<br>Payment amount: " + payment + " - Due: " + endTime +
+                             "<br>APR: " + dpr + " - Payments left: " + numPayments +
+                             "<br><button onclick=\"makePayment();\">Make Payment<button>" +
+                             "</div></li>");
     }
-}
